abd2e7bb2e870ab8f3d3e96773c4faa1ececc6cf
yuma-matsumura <y_sp1_der_212@icloud.com>
2021-07-08 21:19:52 +0900

    挙動確認　修正依頼１

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, expect: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :prepare_prototype, except: [:index, :show]
+  before_action :contributor_action, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.all
@@ -55,4 +57,11 @@ private
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def prepare_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_action
+    redirect_to root_path unless current_user == @prototype.user
+  end
 end
